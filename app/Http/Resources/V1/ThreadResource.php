a385ae6f9ae399ec58f338234438bff3d69af872
Paolo Rizzello <paolo.rizzello@gmail.com>
2021-02-07 23:10:21 +0100

    feat: Updated ThreadResource: added comments relationship id eager loaded

--- app/Http/Resources/V1/ThreadResource.php
+++ app/Http/Resources/V1/ThreadResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\V1;
 
+use App\Http\Resources\V1\Thread\CommentCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ThreadResource extends JsonResource
@@ -18,6 +19,9 @@ class ThreadResource extends JsonResource
             'id'    => $this->id,
             'title' => $this->title,
             'body'  => $this->body,
+            'comments' => new CommentCollection(
+                $this->whenLoaded('comments')
+            ),
         ];
     }
 }
